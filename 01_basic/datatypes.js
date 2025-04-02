efa2e4e604d7ad97fefde19f72c4eb44eafc4c19
Avith97 <139339810+Avith97@users.noreply.github.com>
2025-04-02 13:06:26 +0000

    data types

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,19 @@
+//Stack (Primitive) ,Heap (Non-Primitive)
+
+let myYoutubeName = "avith"
+let another=myYoutubeName
+myYoutubeName ="laxman";
+
+//console.log(another)
+
+let user ={
+     email: "user@google.com",
+      upi: "user@ybl"
+}
+
+let user2 =user;
+user.email ="yahooo"
+
+console.log(user2)
+console.log(user)
+
